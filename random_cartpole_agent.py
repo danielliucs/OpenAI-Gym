da8b6a19739e25900dc2d0ca131d1ca82acddf2e
danielliucs <70533174+danielliucs@users.noreply.github.com>
2022-05-03 12:38:18 -0400

    Update random_cartpole_agent.py

--- random_cartpole_agent.py
+++ random_cartpole_agent.py
@@ -1,20 +1,38 @@
-import gym
+#The purpose of this program is to intervene with the stream of actions sent by agent
+#and insert a probability of 10% of replacing current action with a random one to explore the 
+#environement more
 
+
+import gym
+import random as r
+
+
+class RandomActionWrapper(gym.ActionWrapper):
+    
+    def __init__(self, env, epsilon = 0.1):
+        super(RandomActionWrapper, self). __init__(env) #Uses parent's init method
+        self.epsilon = epsilon #saves a probability of random action
+
+    #Override parent's class method to tweak action to accept a random action   
+    def action(self, action):
+        if r.random() < self.epsilon:
+            print("Random!")
+            return self.env.action_space.sample()
+        return action
 
 if __name__ == "__main__":
-    env = gym.make("CartPole-v1") #creates environment
-    #counters for number of steps and awards
-    total_reward = 0.0 
-    total_steps = 0
-    #always have to reset on first observation, not used since agent is stochastic
+    env = RandomActionWrapper(gym.make("CartPole-v1"))
+
+    #Applying wrapper by creating normal CartPole environemnt and pass to wrapper constructor
+
     obs = env.reset()
+    total_reward = 0.0
 
     while True:
-        action = env.action_space.sample() #sample random item from action space
-        obs, reward, done, extra_info = env.step(action) #returned next observation, local reward, episode done flag, and extra_info abt env
-        total_reward += reward #accumulate total award
-        total_steps += 1 #count number if steps
-        if done: #if end of episode
+        obs, reward, done, extra_info = env.step(0) #uses action of wrapper wehn calling step()
+        total_reward += reward 
+        if done:
             break
     
-    print("Episode done in %d steps, total reward is %.2f" % (total_steps, total_reward))
+    print("Reward got: %.2f" %total_reward)
+
